a267cb187051c7bf16bddc30aed89f17f9facb75
alex <sasha17@live.ru>
2020-07-18 13:12:17 +0300

    IDE-helper dev install

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Schema::defaultStringLength(191);
+
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+
     }
 
     /**
